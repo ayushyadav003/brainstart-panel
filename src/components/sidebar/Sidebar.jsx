2d3d03ce975cab0a692690fb6c3a3fcaa6236330
unknown <developer@lrsservices.in>
2024-02-07 18:12:04 +0530

    sidebar update

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -8,26 +8,26 @@ import {
   Ballot,
   SupportAgent,
   NotificationsActive,
-} from '@mui/icons-material/'
-import './sidebar.scss'
-import { Link, useLocation } from 'react-router-dom'
+} from "@mui/icons-material/";
+import "./sidebar.scss";
+import { Link, useLocation } from "react-router-dom";
 
 const sidebarOptions = [
-  { title: 'Dashboard', icon: <Dashboard />, link: '/a/dashboard' },
-  { title: 'Classes', icon: <LibraryBooks />, link: '/a/classes' },
-  { title: 'Meetings', icon: <VideoCall />, link: '/a/meetings' },
-  { title: 'Students', icon: <School />, link: '/a/students' },
-  { title: 'Notes', icon: <Description />, link: '/a/notes' },
-  { title: 'Attendance', icon: <Ballot />, link: '/a/attendance' },
-  { title: 'Accounts', icon: <PointOfSale />, link: '/a/accounts' },
-  { title: 'Support', icon: <SupportAgent />, link: '/a/support' },
-  { title: 'Notice', icon: <NotificationsActive />, link: '/a/notice' },
-]
+  { title: "Dashboard", icon: <Dashboard />, link: "/a/dashboard" },
+  { title: "Classes", icon: <LibraryBooks />, link: "classes" },
+  { title: "Meetings", icon: <VideoCall />, link: "/a/meetings" },
+  { title: "Students", icon: <School />, link: "/a/students" },
+  { title: "Notes", icon: <Description />, link: "/a/notes" },
+  { title: "Attendance", icon: <Ballot />, link: "/a/attendance" },
+  { title: "Accounts", icon: <PointOfSale />, link: "/a/accounts" },
+  { title: "Support", icon: <SupportAgent />, link: "/a/support" },
+  { title: "Notice", icon: <NotificationsActive />, link: "/a/notice" },
+];
 
 function Sidebar() {
-  const pathname = useLocation()
+  const pathname = useLocation();
 
-  console.log('pathname')
+  console.log("pathname");
 
   return (
     <div className="sidebarContainer">
@@ -35,25 +35,25 @@ function Sidebar() {
       <div className="sidebarOptions">
         {sidebarOptions.map((option, i) => {
           return (
-            <Link href={option.link} key={i}>
+            <Link to={option.link} key={i}>
               <div
-                className={'options'}
+                className={"options"}
                 style={{
                   color:
                     option.title.toLowerCase() === pathname
-                      ? '#fff'
-                      : 'lightgray',
+                      ? "#fff"
+                      : "lightgray",
                 }}
               >
                 <span>{option.icon}</span>
                 <p>{option.title}</p>
               </div>
             </Link>
-          )
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
