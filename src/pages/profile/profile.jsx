f700ee588f7371a48c541b6ce02e1dd7b2cbc932
Udhav-Negi <udhavnegi960@gmail.com>
2024-07-31 22:44:41 +0530

    creating home page ui

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,11 +1,28 @@
 import { Box } from '@mui/material'
+import {Email, Phone} from '@mui/icons-material'
 import React from 'react'
+
+import './Profile.scss'
 
 const Profile = () => {
   return (
-    <Box sx={{border : "3px solid red"}}>
+    <div className="profile">
+      <div className="dp">
+        <img src="/account.png" />
+        <p>Udhav Negi</p>
+        <div className="contact">
+          <p className='email' style={{display : "flex", gap : "8px"}}><span><Email></Email></span>udhavnegi960@gmail.com</p>
+          <p className='phone' style={{display : "flex", gap : "3px"}}><span><Phone></Phone></span>+91-7456904822</p>
+        </div>
+      </div>
 
-    </Box>
+      <div className="actions">
+        <p>Account settings</p>
+        <p>Manage password</p>
+        <p>Address</p>
+        <p>Notifications</p>
+      </div>
+    </div>
   )
 }
 
