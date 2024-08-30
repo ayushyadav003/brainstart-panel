d9b78c412f2c9871795aa022861eed714aae39d5
Ayush <ayush.yadav.100100@gmail.com>
2024-08-30 17:58:21 +0530

    update

--- src/services/ApiConfig.jsx
+++ src/services/ApiConfig.jsx
@@ -1,6 +1,6 @@
 // const apiUrl = 'https://brainstarapi.azurewebsites.net'
-// const apiUrl = "https://brainstar.azurewebsites.net/api/v1";
-const apiUrl = 'http://localhost:8000/api/v1'
+// const apiUrl = "https://brainstar-node-be.vercel.app/api/v1";
+const apiUrl = "http://localhost:8000/api/v1";
 
 export const apiConfig = {
   //auth
@@ -22,4 +22,4 @@ export const apiConfig = {
 
   //meetings
   meetings: `${apiUrl}/meetings`,
-}
+};
